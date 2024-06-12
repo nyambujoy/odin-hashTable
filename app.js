0be6dbd0b918d758fb90dc36416719ffd5f05928
nyambujoy <joyliznyambura@gmail.com>
2024-06-12 12:10:01 +0300

    completed clear func

---
+++ app.js
@@ -0,0 +1,114 @@
+class HashMap {
+    constructor(size) {
+        this.table = new Array(size);
+        this.size = size
+        this.numOfItems = 0
+    }
+    hash(key) {
+        let hashCode = 0
+        for (let i = 0; i < key.length; i++) {
+            hashCode += key.charCodeAt(i)
+        }
+        return hashCode % this.table.length
+    }
+    set(key, value) {
+        const index = this.hash(key);
+        if (!this.table[index]) {
+            this.table[index] = []
+        }
+        let samekey = this.table[index].find(item => item[0] === key)
+        if (samekey) {
+            samekey[1] = value
+        } else {
+            this.table[index].push([key, value])
+            this.numOfItems++
+
+        }
+
+    }
+    display() {
+        for (let i = 0; i < this.table.length; i++) {
+            if (this.table[i]) {
+                console.log(i, this.table[i])
+                // const items = this.table[i].map(item => `(${item[0]}: ${item[1]})`).join(', ');
+                // console.log(`${i}: ${items}`);
+            }
+        }
+    }
+    get(key) {
+        const index = this.hash(key)
+        if (!this.table[index]) {
+            return undefined
+        }
+        let samekey = this.table[index].find(item => item[0] === key)
+        if (samekey) {
+            return samekey[1]
+        } else {
+            return null
+        }
+    }
+    length() {
+        return this.numOfItems
+    }
+    keys() {
+        let keysArr = []
+
+        for (let i = 0; i < this.table.length; i++) {
+            if (this.table[i]) {
+                for (let j = 0; j < this.table[i].length; j++) {
+                    keysArr.push(this.table[i][j][0])
+                }
+            }
+        }
+        return keysArr
+    }
+    values() {
+        let valsArr = []
+        for (let i = 0; i < this.table.length; i++) {
+            if (this.table[i]) {
+                for (let j = 0; j < this.table[i].length; j++) {
+
+                    valsArr.push(this.table[i][j][1])
+
+                }
+            }
+        }
+        return valsArr
+    }
+    remove(key) {
+        const index = this.hash(key)
+        if (!this.table[index]) {
+            return undefined
+        }
+        let itemIndex = this.table[index].findIndex((item) => item[0] === key)
+        if (itemIndex === -1) {
+            return false
+        } else {
+            this.table[index].splice(itemIndex, 1)
+            this.numOfItems--
+            return true
+
+        }
+    }
+    clear() {
+        this.table = new Array(this.size)
+        this.numOfItems = 0
+    }
+}
+
+const hashTable = new HashMap()
+hashTable.set('name', 'Joy')
+hashTable.set('age', 23)
+hashTable.display()
+// console.log(hashTable.get('name'))
+console.log(hashTable.length())
+console.log(hashTable.keys())
+console.log(hashTable.values())
+console.log(hashTable.remove('age'))
+hashTable.display()
+hashTable.clear()
+hashTable.display()
+console.log(hashTable.length())
+
+
+
